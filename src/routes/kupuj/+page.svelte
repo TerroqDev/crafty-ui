9dd9c550389f881dd09ec1abdcead86414081eb5
valentin994 <valentin.vareskic@gmail.com>
2024-10-16 19:20:17 +0200

    added animation

--- src/routes/kupuj/+page.svelte
+++ src/routes/kupuj/+page.svelte
@@ -6,7 +6,8 @@
 	let values = [35, 85];
 
 	let isPriceOpen = true;
-	let isColorOpen = false;
+	let isColorOpen = true;
+	let isSizeOpen = true;
 </script>
 
 <div class=" ">
@@ -15,9 +16,22 @@
 			<h1 class="text-xl text-gray-600 mx-4 py-4 border-b border-gray-600 font-bold">Filter by</h1>
 			<div class="mx-4 pt-4">
 				<p class="text-gray-600 font-semibold flex justify-between">
-					Price <span class="cursor-pointer" on:click={() => (isPriceOpen = !isPriceOpen)}>-</span>
+					Price <span class="cursor-pointer" on:click={() => (isPriceOpen = !isPriceOpen)}>
+						<div class="relative w-3 h-3 cursor-pointer group mt-2">
+							<!-- Vertical line -->
+							<div
+								class={`absolute top-0 left-1/2 w-[2px] h-full bg-gray-600 transform -translate-x-1/2 transition-transform duration-300 ease-out ${isPriceOpen ? 'rotate-90' : ''} `}
+							></div>
+
+							<!-- Horizontal line -->
+							<div
+								class={`absolute top-1/2 left-0 w-full h-[2px] bg-gray-600 transform -translate-y-1/2 transition-transform duration-300 ease-out ${isPriceOpen ? 'rotate-180' : ''} `}
+							></div>
+						</div>
+					</span>
 				</p>
 			</div>
+
 			<div
 				class={`px-2 transform transition-all duration-300 overflow-hidden ${isPriceOpen ? 'max-h-0 ' : 'max-h-40'} `}
 			>
@@ -36,24 +50,72 @@
 			<hr class="bg-gray-600 mx-4 mt-4" />
 			<div class="mx-4 pt-4">
 				<p class="text-gray-600 font-semibold flex justify-between">
-					Color <span class="cursor-pointer" on:click={() => (isColorOpen = !isColorOpen)}>-</span>
+					Size <span class="cursor-pointer" on:click={() => (isSizeOpen = !isSizeOpen)}>
+
+						<div class="relative w-3 h-3 cursor-pointer group mt-2">
+							<!-- Vertical line -->
+							<div
+								class={`absolute top-0 left-1/2 w-[2px] h-full bg-gray-600 transform -translate-x-1/2 transition-transform duration-300 ease-out ${isSizeOpen ? 'rotate-90' : ''} `}
+							></div>
+
+							<!-- Horizontal line -->
+							<div
+								class={`absolute top-1/2 left-0 w-full h-[2px] bg-gray-600 transform -translate-y-1/2 transition-transform duration-300 ease-out ${isSizeOpen ? 'rotate-180' : ''} `}
+							></div>
+						</div>
+                        </span>
+				</p>
+			</div>
+			<div
+				class={`px-4 transform transition-all duration-300 overflow-hidden ${isSizeOpen ? 'max-h-0 ' : 'max-h-40'} `}
+			>
+				<div class="pt-2">
+					<input id="100ml" type="checkbox" />
+					<label for="100ml">100 ml</label>
+				</div>
+				<div class="pt-2">
+					<input id="200ml" type="checkbox" />
+					<label for="200ml">200 ml</label>
+				</div>
+				<div class="pt-2">
+					<input id="300ml" type="checkbox" />
+					<label for="300ml">300 ml</label>
+				</div>
+			</div>
+			<hr class="bg-gray-600 mx-4 mt-4" />
+			<div class="mx-4 pt-4">
+				<p class="text-gray-600 font-semibold flex justify-between">
+					Color <span class="cursor-pointer" on:click={() => (isColorOpen = !isColorOpen)}>
+
+						<div class="relative w-3 h-3 cursor-pointer group mt-2">
+							<!-- Vertical line -->
+							<div
+								class={`absolute top-0 left-1/2 w-[2px] h-full bg-gray-600 transform -translate-x-1/2 transition-transform duration-300 ease-out ${isColorOpen ? 'rotate-90' : ''} `}
+							></div>
+
+							<!-- Horizontal line -->
+							<div
+								class={`absolute top-1/2 left-0 w-full h-[2px] bg-gray-600 transform -translate-y-1/2 transition-transform duration-300 ease-out ${isColorOpen ? 'rotate-180' : ''} `}
+							></div>
+						</div>
+                        </span>
 				</p>
 			</div>
 			<div
 				class={`px-4 transform transition-all duration-300 overflow-hidden ${isColorOpen ? 'max-h-0 ' : 'max-h-40'} `}
 			>
-                <div class="pt-2">
-                    <input id="black" type="checkbox" />
-                    <label for="black">Black</label>
-                </div>
-                <div class="pt-2">
-                    <input id="red" type="checkbox" />
-                    <label for="red">Red</label>
-                </div>
-                <div class="pt-2">
-                    <input id="green" type="checkbox" />
-                    <label for="green">Green</label>
-                </div>
+				<div class="pt-2">
+					<input id="black" type="checkbox" />
+					<label for="black">Black</label>
+				</div>
+				<div class="pt-2">
+					<input id="red" type="checkbox" />
+					<label for="red">Red</label>
+				</div>
+				<div class="pt-2">
+					<input id="green" type="checkbox" />
+					<label for="green">Green</label>
+				</div>
 			</div>
 			<hr class="bg-gray-600 mx-4 mt-4" />
 		</div>
