e5a5b2c3719dcf225ddb22a1a4711d8f55068ece
valentin994 <valentin.vareskic@gmail.com>
2024-10-25 14:22:00 +0200

    updated footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
-<div class="bg-gray-700 text-gray-100">
-	<div class="grid grid-cols-1 pb-4 md:grid-cols-3 gap-4">
-		<div class="px-4 pt-8 md:px-80">
+<div class="bg-gray-700 text-gray-100 md:px-80">
+	<div class="grid grid-cols-1 pb-4 md:grid-cols-4 gap-4">
+		<div class="px-4 pt-8">
 			<a href="/" class="">
 				<svg
 					class="w-48 h-20"
@@ -41,10 +41,52 @@
 			<h1 class="text-lg font-bold text-primary">Linkovi</h1>
 			<ul class="pt-4 flex flex-col gap-2">
 				<li><a href="/kupuj" class="hover:underline">Kupuj</a></li>
-				<li>Moj profil</li>
-				<li>Craftko</li>
-				<li>Q & A</li>
-				<li>Kontakt</li>
+				<li><a href="/mojprofil" class="hover:underline">Moj profil</a></li>
+				<li><a href="/kraftko" class="hover:underline">Kraftko</a></li>
+				<li><a href="/q&a" class="hover:underline">Q & A</a></li>
+				<li><a href="/kontakt" class="hover:underline">Kontakt</a></li>
+			</ul>
+		</div>
+		<div class="pt-4 md:pt-16 px-4">
+			<h1 class="text-lg font-bold text-primary">Pratite nas</h1>
+			<div class="flex gap-2 pt-2">
+				<div class="max-w-8">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						x="0px"
+						y="0px"
+						width="100%"
+						height="100%"
+						viewBox="0 0 30 30"
+					>
+						<path
+							fill="#4b5563"
+							d="M15,3C8.373,3,3,8.373,3,15c0,6.016,4.432,10.984,10.206,11.852V18.18h-2.969v-3.154h2.969v-2.099c0-3.475,1.693-5,4.581-5 c1.383,0,2.115,0.103,2.461,0.149v2.753h-1.97c-1.226,0-1.654,1.163-1.654,2.473v1.724h3.593L19.73,18.18h-3.106v8.697 C22.481,26.083,27,21.075,27,15C27,8.373,21.627,3,15,3z"
+						></path>
+					</svg>
+				</div>
+				<div class="max-w-8">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						x="0px"
+						y="0px"
+						width="100%"
+						height="100%"
+						viewBox="0 0 30 30"
+					>
+						<path
+							fill="#4b5563"
+							d="M 9.9980469 3 C 6.1390469 3 3 6.1419531 3 10.001953 L 3 20.001953 C 3 23.860953 6.1419531 27 10.001953 27 L 20.001953 27 C 23.860953 27 27 23.858047 27 19.998047 L 27 9.9980469 C 27 6.1390469 23.858047 3 19.998047 3 L 9.9980469 3 z M 22 7 C 22.552 7 23 7.448 23 8 C 23 8.552 22.552 9 22 9 C 21.448 9 21 8.552 21 8 C 21 7.448 21.448 7 22 7 z M 15 9 C 18.309 9 21 11.691 21 15 C 21 18.309 18.309 21 15 21 C 11.691 21 9 18.309 9 15 C 9 11.691 11.691 9 15 9 z M 15 11 A 4 4 0 0 0 11 15 A 4 4 0 0 0 15 19 A 4 4 0 0 0 19 15 A 4 4 0 0 0 15 11 z"
+						></path>
+					</svg>
+				</div>
+			</div>
+		</div>
+		<div class="pt-4 md:pt-16 px-4">
+			<h1 class="text-lg font-bold text-primary">Dokumenti</h1>
+			<ul class="pt-4 flex flex-col gap-2">
+				<li><a href="/kupuj" class="hover:underline">Impressum</a></li>
+				<li><a href="/mojprofil" class="hover:underline">Zastita potrosaca</a></li>
 			</ul>
 		</div>
 	</div>
