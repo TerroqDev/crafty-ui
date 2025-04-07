fec5f72c4db5be22a83b38ff39220554ae94fc2f
Marija Matijevic <marija.matijevi@gmail.com>
2025-04-07 21:23:53 +0200

    NOREF: Let nginx handlle full paths

--- src/routes/prijava/+page.svelte
+++ src/routes/prijava/+page.svelte
@@ -28,7 +28,7 @@
 		if (updateFirstName === $ctx.firstName && updateLastName === ctx.lastName) {
 			return;
 		}
-		const request = await fetch(`http://crafty-service.why-ai.net/users/${$ctx.email}`, {
+		const request = await fetch(`/users/${$ctx.email}`, {
 			method: 'PUT',
 			headers: {
 				'Content-Type': 'application/json'
